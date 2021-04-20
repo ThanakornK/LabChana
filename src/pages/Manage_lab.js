5631db98dbc114bc84df969a23073e4b3a59268a
ThanakornK <thanakorn.kit@ku.th>
2021-04-20 07:42:27 +0700

    Revert "hotfix"
    This reverts commit d93b260b7b8571378e07d21e25964f5283cda999.

--- src/pages/Manage_lab.js
+++ src/pages/Manage_lab.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
-import { ListGroup, Button,  Form } from 'react-bootstrap'
+import { ListGroup, Button, Container, Form } from 'react-bootstrap'
 import Sidebar from '../component/Sidebar';
+
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import './../style/Table.css'
 
 export default class Manage_prob extends Component {
@@ -34,16 +38,19 @@ export default class Manage_prob extends Component {
                         </Form.Group>
                     </Form>
                     <br />
-                    <Form>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Status</Form.Label>
-                            <Form.Control as="select">
-                                <option>Open</option>
-                                <option>Disable</option>
-                                <option>Close</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Form>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Switch color="primary" checked={this.state.openChecked} onChange={() => this.setState({openChecked: !(this.state.openChecked)})} />}
+                            label="Open"
+                        />
+                    </FormGroup>
+                    <br />
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Switch color="primary" checked={this.state.availableChecked} onChange={() => this.setState({ availableChecked: !(this.state.availableChecked) })} />}
+                            label="Available"
+                        />
+                    </FormGroup>
                     <br />
                     {/* <h4>Available</h4>
                     <br />
